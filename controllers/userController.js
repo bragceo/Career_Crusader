59b0b49568c924b2b03203bf8bae31d1f0315e87
bragceo <119948453+bragceo@users.noreply.github.com>
2023-05-03 08:33:13 -0400

    revise controller job code

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,32 +12,89 @@ const router = express.Router();
 // @desc    Register a user
 // @access  Public
 router.post('/register', async (req, res) => {
-    const { email, password } = req.body;
-  
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ where: { email } });
+
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = await User.create({
+      email,
+      password: bcrypt.hashSync(password, 10)
+    });
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   POST api/users/login
+// @desc    Authenticate user and get token
+// @access  Public
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid email or password' });
+    }
+
+    const isMatch = bcrypt.compareSync(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid email or password' });
+    }
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   GET api/users
+// @desc    Get user by token
+// @access  Private
+router.get('/', auth, async (req, res) => {
     try {
-      let user = await User.findOne({ where: { email } });
+      const user = await User.findByPk(req.user.id, { attributes: { exclude: ['password'] } });
   
-      if (user) {
-        return res.status(400).json({ msg: 'User already exists' });
+      if (!user) {
+        return res.status(404).json({ msg: 'User not found' });
       }
   
-      user = await User.create({
-        email,
-        password: bcrypt.hashSync(password, 10)
-      });
-  
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
-  
-      jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      });
+      res.json(user);
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
   });
+  
+  module.exports = router;
+  
+
